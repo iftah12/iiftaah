931feaac6038d4663696b4cfb23c8efdd34a8201
iftah <iftahfadlilah2000@gmail.com>
2021-11-24 11:01:56 +0700

    isi

--- main/views.py
+++ main/views.py
@@ -6,4 +6,10 @@ def home(request):
         'judul':'kuliah pak huda',
         'kontributor':'Iftah Nur Fadlilah',
     }
+    if request.method=='POST':
+        print('ini method post')
+        context['nama']=request.POST['nama']
+        context['nim'] = request.POST['nim']
+    else:
+        print('ini method get')
     return render(request, 'base.html',context)
